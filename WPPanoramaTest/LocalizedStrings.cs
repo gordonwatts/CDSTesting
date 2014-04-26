5b888468d8a2a6f69710d10f613b0bc6969349f7
Gordon Watts <gordon@gordonandpaula.net>
2014-04-26 15:52:53 -0700

    What does a panorama look like?

---
+++ WPPanoramaTest/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WPPanoramaTest.Resources;
+
+namespace WPPanoramaTest
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
